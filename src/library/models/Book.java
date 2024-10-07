f64367d199010cacb2c702579b2704988fccc223
kartem777 <111637570+kartem777@users.noreply.github.com>
2024-10-07 13:01:56 +0200

    Update Book.java

--- src/library/models/Book.java
+++ src/library/models/Book.java
@@ -1,4 +1,40 @@
 package library.models;
 
-public class Book {
+public class Book extends LibraryItem implements Borrowable {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true;
+    }
+
+    public String getBookInfo() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn + ", Available: " + isAvailable;
+    }
+
+    public void borrow() {
+        if (isAvailable) {
+            isAvailable = false;
+        } else {
+            throw new BookUnavailableException("The book is currently unavailable.");
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+    }
+
+    @Override
+    public String getItemType() {
+        return "Book";
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
