7791fb28d4ba8102fda79890ac26ea63cfe4a337
kartem777 <111637570+kartem777@users.noreply.github.com>
2024-10-07 13:02:50 +0200

    Update Library.java

--- src/library/services/Library.java
+++ src/library/services/Library.java
@@ -1,4 +1,26 @@
 package library.services;
 
-public class Library {
+import library.models.LibraryItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library<T extends LibraryItem> {
+    private List<T> items;
+
+    public Library() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
 }
