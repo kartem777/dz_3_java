39893086db8dc8921945ace219eaafe2611f0a76
kartem777 <111637570+kartem777@users.noreply.github.com>
2024-10-07 13:02:38 +0200

    Update User.java

--- src/library/models/User.java
+++ src/library/models/User.java
@@ -1,4 +1,46 @@
 package library.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private int userId;
+    private List<Book> borrowedBooks;
+
+    public User(String name, int userId) {
+        this.name = name;
+        this.userId = userId;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public void borrowBook(Book book) {
+        if (book.isAvailable()) {
+            book.borrow();
+            borrowedBooks.add(book);
+        } else {
+            throw new BookUnavailableException("Book is not available.");
+        }
+    }
+
+    public void returnBook(Book book) {
+        if (borrowedBooks.contains(book)) {
+            book.returnBook();
+            borrowedBooks.remove(book);
+        } else {
+            System.out.println("This user hasn't borrowed this book.");
+        }
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
 }
