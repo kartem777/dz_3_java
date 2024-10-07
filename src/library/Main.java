14cc2508ae45d5b8256eff543b889d94f5bc4675
kartem777 <111637570+kartem777@users.noreply.github.com>
2024-10-07 13:03:20 +0200

    Update Main.java

--- src/library/Main.java
+++ src/library/Main.java
@@ -1,7 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package library;
+import library.models.*;
+import library.services.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Library<Book> library = new Library<>();
+        List<User> users = new ArrayList<>();
+        LibraryService libraryService = new LibraryService(library, users);
 
+        Book book1 = new Book("aaadsfasdaa", "a", "12345");
+        Book book2 = new Book("adfsaadsf", "b", "123");
+
+        libraryService.addBook(book1);
+        libraryService.addBook(book2);
+
+        User user1 = new User("user1", 1);
+        User user2 = new User("user2", 2);
+
+        users.add(user1);
+        users.add(user2);
+
+        try {
+            libraryService.lendBook(user1, book1);
+            System.out.println(user1.getName() + " borrowed: " + book1.getBookInfo());
+            libraryService.lendBook(user2, book1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        libraryService.returnBook(user1, book1);
+        System.out.println(user1.getName() + " returned: " + book1.getBookInfo());
+
+        System.out.println("Available books: ");
+        libraryService.getAvailableBooks().forEach(book -> System.out.println(book.getBookInfo()));
     }
 }
